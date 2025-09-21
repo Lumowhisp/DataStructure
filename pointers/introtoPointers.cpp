4effb63baa858bb6ee18dc15da130b5981c6102f
Lumowhisp <kumaraditya12981006@gmail.com>
2025-09-21 22:03:31 +0530

    Pointers

---
+++ pointers/introtoPointers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num=5;
+    cout<<num<<endl;
+    //address of num
+    cout <<"Address :" <<&num<<endl;
+    //pointer store address
+
+return 0;
+}
